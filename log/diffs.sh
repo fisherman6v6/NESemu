f6910f0cbbecd24ca5bd32fef0358eb01b84d3b2
Alessandro Fici <38260190+fisherman6v6@users.noreply.github.com>
2020-12-18 13:15:12 +0100

    Update diffs.sh

--- log/diffs.sh
+++ log/diffs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#little script to compute the differences between the original nestest log and the log produced by my emu. Only the PC is take into account.
+#little script to compute the differences between the original nestest log and the log produced by my emu. Only the PC is taken into account.
 awk '{print $1}' ~/Documents/Work/NESemu/log/nestest_log.txt > ~/Documents/Work/NESemu/log/PC_nestest_log.txt
 awk '{print $1}' ~/Documents/Work/NESemu/log/my_log.txt > ~/Documents/Work/NESemu/log/PC_my_log.txt
 
